1349b32570831de63848cb430d5f4f0bd77fd167
rn-mnsl <manansalarin@gmail.com>
2023-09-28 22:20:36 +0800

    commit

---
+++ src/variable.java
@@ -0,0 +1,17 @@
+public class variable {
+    public static void main(String[] args){
+        char var1 = '9';
+        String var2 = "Hi, My name is Roan!";
+        boolean var3 = true;
+        int var4 = 900;
+        double var5 = 1.23;
+
+
+        System.out.println(var1 + var2 + var3 + var4 + var5);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+    }
+
+}
